c488339a7eec05bd952d49530d96fbb32bdf2b9b
Tyller Mendoza <tyllermendoza@gmail.com>
2023-11-15 22:54:30 -0500

    quick update 2

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -9,7 +9,7 @@ const cn = {
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
   user: process.env.PG_USER,
-  //password: process.env.PG_PASSWORD
+  password: process.env.PG_PASSWORD
 };
 
 // connect to songs_dev db
